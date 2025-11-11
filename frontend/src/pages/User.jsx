d2b7351d8bebc0b2f43e77374775075b70110032
SHIJU A <118278088+shijuvtm@users.noreply.github.com>
2025-11-11 13:40:40 +0530

    Display user name in User Dashboard

--- frontend/src/pages/User.jsx
+++ frontend/src/pages/User.jsx
@@ -1,11 +1,19 @@
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+
 export default function User() {
+  const { user } = useContext(AuthContext);
+
   return (
-    <div className="text-center mt-20">
-      <h1 className="text-3xl font-bold text-blue-600">User Dashboard</h1>
-      <p className="mt-3 text-white-700">Welcome to your dashboard.</p>
+    <div className="flex flex-col items-center justify-center mt-5">
+     
+      <h1 className="text-3xl font-bold text-blue-600">
+        👋 Hi, {user?.user?.name || "User"}
+      </h1>
+
+      <p className="text-gray-700 mt-2 text-lg">
+        Welcome back to your dashboard!
+      </p>
     </div>
   );
 }
-
-
-
